2cdf08be769cdc6d7f781e749bb6c96b75c1cab6
OvalleGA <OvalleGA@CO-OVALLEGA.lac.uis.unisys.com>
2019-04-23 16:12:22 -0500

    Versión del template Web con funcionalidad y seguridad de usuario. Previo a la implementacion del controlador de menu y seguridades.

--- src/main/java/org/primefaces/customize/controllers/session/LoginUsers.java
+++ src/main/java/org/primefaces/customize/controllers/session/LoginUsers.java
@@ -5,9 +5,7 @@
  */
 package org.primefaces.customize.controllers.session;
 
-import java.util.HashMap;
 import javax.servlet.http.HttpSession;
-import org.primefaces.customize.UI.exceptions.ManhattanException;
 
 /**
  *
@@ -23,25 +21,21 @@ public class LoginUsers {
         return instance;
     }
     
-    public boolean ValidateCredentials(String user, String password, HttpSession session) throws ManhattanException{
+    public boolean ValidateCredentials(String user, String password, HttpSession session) throws Exception{
         String msg ="No se pudo iniciar sesión porque: ";
-        if(user == null || password == null || 
-           user.equals("") || password.equals("")
-           || UserSessionManager.getInstance().isUserConnected(session)
-           || !isInvalidCrendetial(user, password)){
-            msg = msg+(user == null?"\n - El usuario no puede ser nulo":"")
-                     +(password == null?"\n - La contraseña no puede ser nula":"")
-                     +(user.equals("")?"\n - El usuario no puede estar vacio":"")
-                     +(password.equals("")?"\n - La contraseña no puede estar vacia":"")
-                     +(UserSessionManager.getInstance().isUserConnected(session)?"\n - El usuario "+user+" ya tiene una sesión abierta.":"")
-                     +(password.equals("")?"\n - El usuario y/o contraseña no son validos.":"");
-            throw new ManhattanException(msg);
+        msg = msg+(user == null?"\n - El usuario no puede ser nulo":"")
+                 +(password == null?"\n - La contraseña no puede ser nula":"")
+                 +(UserSessionManager.getInstance().isUserConnected(session)?"\n - El usuario "+user+" ya tiene una sesión abierta.":"")
+                 +(!isInvalidCrendetial(user, password)?"\n - El usuario y/o contraseña no son validos.":"");
+        System.out.println("El mensaje es: "+msg);
+        if(!msg.equals("No se pudo iniciar sesión porque: ")){
+            throw new Exception(msg);
         }
         UserSessionManager.getInstance().connectUser(user, session);
         return true;
     }
 
-    private static boolean isInvalidCrendetial(String user, String password) {
+    private static boolean isInvalidCrendetial(String user, String password) throws Exception{
         if(!user.equals("govalle") && !password.equals("ovalle")){
             return false;
         }
